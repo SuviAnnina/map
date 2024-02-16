b1f375bb5393fd1fdaf1153e8fbdf73be9c619ee
SuviAnnina <suvi.varjoranta@gmail.com>
2024-02-16 16:58:44 +0200

    add geolocation fetching

--- App.js
+++ App.js
@@ -1,18 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import MapView from 'react-native-maps'
+import { StyleSheet, View, Button, TextInput, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
 
 export default function App() {
 
   const [address, setAddress] = useState("");
-
-  const mapRegion = {
+  const [coordinates, setCoordinates] = useState({
+    latitude: 60.192059,
+    longitude: 24.945831
+  });
+  const [mapRegion, setMapRegion] = useState({
     latitude: 60.192059,
     longitude: 24.945831,
     latitudeDelta: 0.0322,
-    longitudeDelta: 0.0221,
+    longitudeDelta: 0.0221
+  });
+
+  const apiKey = 'yourApiKey'
+
+  const fetchCoordinates = () => {
+    fetch(`https://geocode.maps.co/search?q=${address}&api_key=${apiKey}`)
+      .then(response => {
+        if (!response)
+          throw new Error("Error in fetch: " + response.statusText);
+        return response.json();
+      })
+      .then(data => {
+        if (data.length > 0) {
+          const firstResult = data[0];
+          const latitudeFloat = parseFloat(firstResult.lat);
+          const longitudeFloat = parseFloat(firstResult.lon);
+
+          setMapRegion({
+            ...mapRegion,
+            latitude: latitudeFloat,
+            longitude: longitudeFloat
+          })
+
+          setCoordinates({
+            latitude: latitudeFloat,
+            longitude: longitudeFloat
+          });
+        } else {
+          Alert.alert("No results found for address " + address);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
+
+
 
 
   return (
@@ -21,7 +60,13 @@ export default function App() {
       <MapView
         region={mapRegion}
         style={styles.mapStyle}
-      />
+      >
+
+        <Marker
+          coordinate={coordinates}
+        />
+      </MapView>
+
 
       <View>
         <TextInput
@@ -31,6 +76,7 @@ export default function App() {
           onChangeText={text => setAddress(text)}
         />
         <Button title="Show"
+          onPress={fetchCoordinates}
         />
       </View>
       <StatusBar style="auto" />

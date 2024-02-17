2b7ef29fe3876b6f7ea68893e15fc2aa576b0437
SuviAnnina <suvi.varjoranta@gmail.com>
2024-02-17 15:52:45 +0200

    Add users current location, env, activityIndicator and some styling changes

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, Alert, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Button, TextInput, Alert, KeyboardAvoidingView, Platform, Keyboard, ActivityIndicator } from 'react-native';
 import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location'
 
 export default function App() {
 
@@ -16,10 +17,13 @@ export default function App() {
     latitudeDelta: 0.0322,
     longitudeDelta: 0.0221
   });
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const apiKey = 'yourApiKey'
-
+  // Fetch coordinates of the given address
   const fetchCoordinates = () => {
+    setLoading(true);
+    const apiKey = process.env.EXPO_PUBLIC_API_KEY;
     fetch(`https://geocode.maps.co/search?q=${address}&api_key=${apiKey}`)
       .then(response => {
         if (!response)
@@ -45,43 +49,70 @@ export default function App() {
 
           Keyboard.dismiss();
           setAddress("");
+          setLoading(false);
         } else {
           Alert.alert("No results found for " + address);
+          setLoading(false);
         }
       })
       .catch(err => {
         console.log(err);
+        setLoading(false);
       })
   }
+
+  // Check permission to get location information
+  // If permission is given, update map to current location
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permission to get location')
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setMapRegion({ ...mapRegion, latitude: location.coords.latitude, longitude: location.coords.longitude });
+      setCoordinates({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+      setLoading(false);
+    })();
+  }, []);
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.container}
-    >
+      style={styles.container}>
 
       <MapView
         region={mapRegion}
-        style={styles.mapStyle}
-      >
+        style={styles.mapStyle}>
         <Marker
-          coordinate={coordinates}
-        />
+          coordinate={coordinates} />
       </MapView>
 
-      <View style={{ width: "100%" }}>
-        <TextInput
-          placeholder="Type an address"
-          style={styles.TextInput}
-          value={address}
-          onChangeText={text => setAddress(text)}
+      <View style={styles.bottomContainer}>
+        {loading ? (
+          <View>
+            <ActivityIndicator size="large" />
+          </View>
+        ) : (
+          <View>
+            <TextInput
+              placeholder="Type an address"
+              style={styles.TextInput}
+              value={address}
+              onChangeText={text => setAddress(text)} />
 
-        />
-        <Button title="Show"
-          onPress={fetchCoordinates}
-        />
+            <Button
+              title="Show"
+              onPress={fetchCoordinates} />
+          </View>
+        )}
+
       </View>
       <StatusBar style="auto" />
+
     </KeyboardAvoidingView>
   );
 }
@@ -96,11 +127,17 @@ const styles = StyleSheet.create({
   TextInput: {
     fontSize: 18,
     textAlign: 'center',
-    marginTop: 20,
   },
   mapStyle: {
     width: "100%",
-    height: "85%",
+    height: "90%",
+  },
+  bottomContainer: {
+    width: "100%",
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '7%'
   },
 
 });
